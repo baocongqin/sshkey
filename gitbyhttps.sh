6192178b8b359c6e2f44780185fef9c5eb650dc5
baocongqin <baocongqin@qq.com>
2024-06-30 13:31:12 +0800

    a3

---
+++ gitbyhttps.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+str="https://github.com/baocongqin/$1.git"
+git clone ${str}
--- gitbyssh.sh
+++ gitbyssh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+# str="https://github.com/baocongqin/$1"
 sshurl="git@github.com:baocongqin/$1.git"
 git clone ${sshurl}
